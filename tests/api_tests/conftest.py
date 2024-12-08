2e68606e55f0fc92c6dacfc28a0f42b4e1083d6b
CvetelinStoimenov <stoimenovv@gmail.com>
2024-12-08 20:33:00 +0200

    fixes

--- tests/api_tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from helpers.cert_helper import fetch_certificate
-
-@pytest.fixture(scope="session", autouse=True)
-def setup_certificates():
-    """Ensure root CA certificate is available before running tests."""
-    cert_path = fetch_certificate()
-    return cert_path
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
+import sys
+print("PYTHONPATH:", sys.path)
 from tests.utils import wait_for_server
-from tests.helpers.cert_helper import fetch_certificate
+from helpers.cert_helper import fetch_certificate
 
 @pytest.fixture(scope="session", autouse=True)
 def wait_for_mock_server():
